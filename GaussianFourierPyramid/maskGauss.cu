1c6fddf7d04dedcea0cb7bd4aa72e158af0f5b86
giacomo carsana <giacomocarsana@digitecinnovation.com>
2025-05-26 08:22:10 +0200

    maskgauss

--- GaussianFourierPyramid/maskGauss.cu
+++ GaussianFourierPyramid/maskGauss.cu
@@ -35,97 +35,14 @@ __global__ void computeWeightedAverageKernel(const uchar* mask, const cv::Point*
 	}
 }
 
-//bool computeWeightedAverage(const uchar* mask, const cv::Point* contourPoints, const float* contourValues,
-//	float* output, int rows, int cols, int numContourPoints, float sigma)
-//{
-//	uchar* d_mask;
-//	cv::Point* d_contourPoints;
-//	float* d_contourValues;
-//	float* d_output;
-//
-//	size_t maskSize = rows * cols * sizeof(uchar);
-//	size_t pointSize = numContourPoints * sizeof(cv::Point);
-//	size_t valueSize = numContourPoints * sizeof(float);
-//	size_t outputSize = rows * cols * sizeof(float);
-//
-//	cudaMallocManaged(&d_mask, maskSize);
-//	cudaMallocManaged(&d_contourPoints, pointSize);
-//	cudaMallocManaged(&d_contourValues, valueSize);
-//	cudaMallocManaged(&d_output, outputSize);
-//
-//	cudaMemcpy(d_mask, mask, maskSize, cudaMemcpyHostToDevice);
-//	cudaMemcpy(d_contourPoints, contourPoints, pointSize, cudaMemcpyHostToDevice);
-//	cudaMemcpy(d_contourValues, contourValues, valueSize, cudaMemcpyHostToDevice);
-//
-//	dim3 blockSize(32, 32);
-//	dim3 gridSize((cols + blockSize.x - 1) / blockSize.x, (rows + blockSize.y - 1) / blockSize.y);
-//
-//	// Ensure that gridSize.x and gridSize.y are within valid limits
-//	if (gridSize.x > 65535 || gridSize.y > 65535) {
-//		std::cout << "Grid size exceeds maximum limit." << std::endl;
-//		return false;
-//	}
-//
-//	cudaDeviceProp prop;
-//	cudaGetDeviceProperties(&prop, 0);
-//	if (blockSize.x * blockSize.y * blockSize.z > (size_t)prop.maxThreadsPerBlock)
-//	{
-//		std::cout << "Max threads per block: " << prop.maxThreadsPerBlock << "\n";
-//		cudaFree(d_mask);
-//		cudaFree(d_contourPoints);
-//		cudaFree(d_contourValues);
-//		cudaFree(d_output);
-//		return false;
-//	}
-//
-//	computeWeightedAverageKernel << <gridSize, blockSize >> > (d_mask, d_contourPoints, d_contourValues, d_output,
-//		rows, cols, numContourPoints, sigma*sigma);
-//
-//	cudaError_t e = cudaGetLastError();
-//	if (e != cudaSuccess)
-//	{
-//		std::cout << "CUDA Launch Error: " << cudaGetErrorString(e) << "\n";
-//		cudaFree(d_mask);
-//		cudaFree(d_contourPoints);
-//		cudaFree(d_contourValues);
-//		cudaFree(d_output);
-//		return false;
-//	}
-//
-//
-//	e = cudaDeviceSynchronize();
-//	if (e != cudaSuccess)
-//	{
-//		std::cout << "CUDA Sync Error: " << cudaGetErrorString(e) << "\n";
-//		cudaFree(d_mask);
-//		cudaFree(d_contourPoints);
-//		cudaFree(d_contourValues);
-//		cudaFree(d_output);
-//		return false;
-//	}
-//
-//
-//	cudaMemcpy(output, d_output, outputSize, cudaMemcpyDeviceToHost);
-//
-//	cudaFree(d_mask);
-//	cudaFree(d_contourPoints);
-//	cudaFree(d_contourValues);
-//	cudaFree(d_output);
-//	return true;
-//}
 
 cv::Mat maskedGaussianGPU(const cv::Mat& grayscale, const cv::Mat& mask, const cv::Mat& contour, float sigma)
 {
 	if (grayscale.empty() || mask.empty() || contour.empty()) 
-	{
-		std::cout << "Could not load images.\n";
-		return cv::Mat();
-	}
+		throw std::runtime_error("Could not load images");
 
-	if (grayscale.type() != CV_32F || mask.type() != CV_8UC1 || contour.type() != CV_8UC1) {
-		std::cout << "Unexpected image types.\n";
-		return cv::Mat();
-	}
+	if (grayscale.type() != CV_32F || mask.type() != CV_8UC1 || contour.type() != CV_8UC1) 
+		throw std::runtime_error("Unexpected image types");
 
 	// Extract contour points
 	std::vector <cv::Point> contourPoints;
@@ -139,14 +56,9 @@ cv::Mat maskedGaussianGPU(const cv::Mat& grayscale, const cv::Mat& mask, const c
 		}
 	}
 
-
-	// computeWeightedAverage(mask.ptr<uchar>(0), &contourPoints[0], &contourValues[0], output.ptr<float>(0), grayscale.rows, grayscale.cols, contourPoints.size(), sigma);
-	
 	const size_t numContourPoints{ contourPoints.size() };
-
 	size_t pointSize = numContourPoints * sizeof(cv::Point);
 	size_t valueSize = numContourPoints * sizeof(float);
-
 
 	cv::Point* d_contourPoints;
 	float* d_contourValues;
@@ -164,8 +76,7 @@ cv::Mat maskedGaussianGPU(const cv::Mat& grayscale, const cv::Mat& mask, const c
 
 	// Ensure that gridSize.x and gridSize.y are within valid limits
 	if (gridSize.x > 65535 || gridSize.y > 65535) {
-		std::cout << "Grid size exceeds maximum limit." << std::endl;
-		return cv::Mat();
+		throw std::runtime_error("Grid size exceeds maximum limit");
 	}
 
 	cudaDeviceProp prop;
@@ -174,22 +85,20 @@ cv::Mat maskedGaussianGPU(const cv::Mat& grayscale, const cv::Mat& mask, const c
 	{
 		cudaFree(d_contourPoints);
 		cudaFree(d_contourValues);
-		std::cout << "Max threads per block: " << prop.maxThreadsPerBlock << "\n";
-		return cv::Mat();
+		throw std::runtime_error("Max threads per block exceeded");
 	}
 	
 	cv::cuda::GpuMat maskGpu(mask);
 	cv::cuda::GpuMat outputGpu(grayscale.size(), CV_32F);
 	computeWeightedAverageKernel << <gridSize, blockSize >> > (maskGpu.ptr<uchar>(0), d_contourPoints, d_contourValues, outputGpu.ptr<float>(0),
-		rows, cols, numContourPoints, sigma * sigma);
+		rows, cols, (int)numContourPoints, sigma * sigma);
 
 	cudaError_t e = cudaGetLastError();
 	if (e != cudaSuccess)
 	{
 		cudaFree(d_contourPoints);
 		cudaFree(d_contourValues);
-		std::cout << "CUDA Launch Error: " << cudaGetErrorString(e) << "\n";
-		return cv::Mat();
+		throw std::runtime_error("CUDA Launch Error");
 	}
 
 	e = cudaDeviceSynchronize();
@@ -197,7 +106,7 @@ cv::Mat maskedGaussianGPU(const cv::Mat& grayscale, const cv::Mat& mask, const c
 	{
 		cudaFree(d_contourPoints);
 		cudaFree(d_contourValues);
-		std::cout << "CUDA Sync Error: " << cudaGetErrorString(e) << "\n";
+		throw std::runtime_error("CUDA Sync Error");
 		return cv::Mat();
 	}
 
